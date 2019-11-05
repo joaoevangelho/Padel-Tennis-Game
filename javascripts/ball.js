ba616ac1093ff1b16d92c13854fd5b5902f9c334
Joao Evangelho <joaoevangelho@MacBook-Pro-de-Joao-3.local>
2019-11-05 11:03:29 +0000

    more changes

--- javascripts/ball.js
+++ javascripts/ball.js
@@ -3,20 +3,27 @@ class Ball {
         this.game = game;
         this.context = game.context;
         this.$canvas = game.$canvas;
-
+        this.img = new Image();
         this.radius = 5;
         this.x = 150;
         this.y = 150;
         this.vx = 10;
         this.vy = 10;
+        this.size = this.radius;
+        this.img.src = "./../images/bola.png";
     }
 
     draw() {
+        /*
         this.context.beginPath();
         this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         this.context.fillStyle = 'yellow';
         this.context.fill();
         this.context.closePath();
+        */
+        this.context.drawImage(this.img, 0, 0, 500, 500, this.x, this.y, 15, 15);
+        this.context.restore();
+
     }
 
     move() {
@@ -31,5 +38,3 @@ class Ball {
         this.context.clearRect(0, 0, $canvas.width, $canvas.height);
     }
 }
-
-
